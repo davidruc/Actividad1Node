be10a803da88872409dc7af2b16522dc452c1dac
davidruc <davidarueda18@gmail.com>
2023-07-08 12:37:32 -0500

    feat: :rocket: EndPoint para mover inventario entre bodegas
    hace falta insertar los datos en historial

--- backend/src/controlles/inventarios.controlles.js
+++ backend/src/controlles/inventarios.controlles.js
@@ -30,20 +30,44 @@ const añadir_o_actualizar_inventario = async (req,res)=>{
 const updateProduct = async(req, res)=>{
     try{
         const {id} = req.params;
-        const {id_bodega, cantidad} = req.body;
+        let {id_bodega, cantidad} = req.body;
         const connection = await getConnection();
         const result = await connection.query("SELECT * FROM inventarios WHERE id = ?", id);
         async function traslados(){
             const busqueda = result.find(val =>(val.cantidad >= cantidad));
-            console.log(busqueda);
             if(busqueda !== undefined){
-                console.log("alcanza");
+                //Esta parte del código lo que hace es restarle a la bodega de la que tomo el producto la cantidad que le estoy mandando a otra bodega. 
+                cantidad = busqueda.cantidad - cantidad ;
+                const inventario = {cantidad}
+                const result2 = await connection.query("UPDATE inventarios SET ? WHERE id = ?", [inventario, id]);
+
+                //Aquí voy a tomar el id_producto y voy a buscar en la base de datos si existe en el inventario el id_bodega que estoy ingresando relacionado con ese producto
+                const id_producto = result[0].id_producto;
+                const ExistenciaP_B = await connection.query("SELECT * FROM inventarios WHERE id_producto = ? AND id_bodega = ?", [id_producto, id_bodega ]); 
+                if (ExistenciaP_B != "") {
+                    //Una vez traidos los datos de la nueva consulta vamos a tomar su id y hacer el update de esta bodega.
+                    const newid = ExistenciaP_B[0].id;
+                    cantidad = ExistenciaP_B[0].cantidad + result[0].cantidad - cantidad; 
+                    const inventario2 = {cantidad};
+                    const pasoNewBodega = await connection.query("UPDATE inventarios SET ? WHERE id = ?", [inventario2, newid]);
+
+                    //Se hacen las consultas para enviar en el json tanto el dato que se le saco el producto y el dato donde se agregó la cantidad que se saco
+                    const result3 = await connection.query("SELECT * FROM inventarios WHERE id = ?", id);
+                    const result4 = await connection.query("SELECT * FROM inventarios WHERE id = ?", newid);
+                    res.json([result3,result4])
+                } else {
+                    //Si la bodega a la que se está intentando meter el producto no lo tiene, crea un nuevo registro y lo añade automáticamente
+                    cantidad = result[0].cantidad - cantidad;
+                    const newInventario = {id_producto, id_bodega, cantidad}
+                    const crearNewRegistro = await connection.query("INSERT INTO inventarios SET ?", newInventario);
+                    console.log("la bodega no tenía este artículo, por lo que se creará");
+                    res.json(crearNewRegistro)
+                }
             } else{
-                console.log("no alcanza el producto");
+                res.json({message: "Estás intentando trasladar demasiados productos."})
             }
         }
         traslados();
-        res.json(result)
     }catch(error){
         res.status(500);
         res.send(error.message);
