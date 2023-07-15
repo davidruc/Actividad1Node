7dac3f51753fc0f3b767802662d24d48693fbd2b
davidruc <davidarueda18@gmail.com>
2023-07-15 16:41:00 -0500

    feat: :fire: eliminación de archivo config.js

--- backend/src/routes/inventarios.routes.js
+++ backend/src/routes/inventarios.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import {consultasInventarios as consultas} from "../controlles/inventarios.controlles.js";
+import mysql from "mysql2";
 
+let con = undefined;
 const router = Router();
 
 router.post("/", consultas.añadir_o_actualizar_inventario);
