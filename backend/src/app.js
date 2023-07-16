913dd12189e051cc09c531f544aa30c4fa56d067
davidruc <davidarueda18@gmail.com>
2023-07-16 17:35:17 -0500

    feat: :fire: se elimina el archivo que no era necesario para rutas

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodegaRoute from "./routes/bodegas.routes.js";
 import routerProduct from "./routes/productos.routes.js";
 import routerInventario from "./routes/inventarios.routes.js";
-/* import routerMovimientoInventario from "./routes/movimientoInventario.routes.js" */
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,7 +11,7 @@ app.use(express.json());
 
 //Routes
 app.use("/api/bodegas", bodegaRoute);
-/* Para el post de bodegas hay que usar las siguientes entradas:
+/* //? Para el post de bodegas hay que usar las siguientes entradas:
   "nombre":"",
   "id_responsable":,
   "estado":,
@@ -20,8 +19,7 @@ app.use("/api/bodegas", bodegaRoute);
 
 app.use("/api/productos/total", routerProduct);
 app.use("/api/insert/productos", routerProduct);
-/* 
- Los parámetros para agregar productos y que se guarde de manera automática el inventario en una bodega default:
+/* //? Los parámetros para agregar productos y que se guarde de manera automática el inventario en una bodega default:
  "nombre":,
  "descripcion":,
  "estado":,
@@ -32,17 +30,13 @@ app.use("/api/insert/productos", routerProduct);
 
 app.use("/api/inventarios", routerInventario);
 app.use("/api/moverInventarios", routerInventario);
-/* 
-  Los parámetros para agregar o actualizar un inventario es: 
+/* //? Los parámetros para agregar o actualizar un inventario es: 
   {
   "id_producto":,
   "id_bodega":,
   "cantidad":
   }
 */
-/* app.use("/api/movimiento", routerMovimientoInventario); */
-
-
 
 
 const config = JSON.parse(process.env.MY_CONFIG);
